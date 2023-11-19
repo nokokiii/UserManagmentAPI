05555a8b95ed5c0386d065e84d72f95a9659cd27
nokokiii <kubabokoki@gmail.com>
2023-11-19 17:05:23 +0100

    404 error handling

--- src/api.py
+++ src/api.py
@@ -2,30 +2,42 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'error': 'Not found', 'message': 'The requested endpoint does not exist.'}), 404
+
+
 @app.route('/users', methods=['GET'])
 def get_all_users():
     # TODO: Implement
     return jsonify()
 
+
 @app.route('/users/<int:user_id>', methods=['GET'])
-def get_user_by_id(user_id):
+def get_user(user_id):
     # TODO: Implement
     return jsonify()
+
 
 @app.route('/users', methods=['POST'])
 def create_user():
     # TODO: Implement
     return jsonify()
 
+
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
     # TODO: Implement
     return jsonify()
+
 
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
     # TODO: Implement
     return jsonify()
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
