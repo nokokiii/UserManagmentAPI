90211dd55fee39e293851170568b23eef693e50d
nokokiii <kubabokoki@gmail.com>
2023-11-27 23:04:58 +0100

    create user

--- src/api.py
+++ src/api.py
@@ -7,27 +7,32 @@ app = Flask(__name__)
 
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({'error': 'Not found', 'message': 'The requested endpoint does not exist.'}), 404
+    return jsonify({'error': 'Not Found', 'message': 'The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.'}), 404
 
 
 @app.route('/users', methods=['GET'])
 def get_all_users():
     if all_users := af.get_all_users():
         return jsonify(all_users), 200
-    return jsonify({'error': 'Intenal error', 'message': 'There was promlem while connecting to database'}), 500
+    return jsonify({'error': 'Intenal Error', 'message': 'There was promlem while connecting to database'}), 500
 
 
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
     if user := af.get_user(user_id):
         return jsonify(user), 200
-    return jsonify({'error': 'Not found', 'message': 'The requested user does not exist.'}), 404
+    return jsonify({'error': 'Not Found', 'message': 'The requested user does not exist.'}), 404
 
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    # TODO: Implement
-    return jsonify(), 501
+    data = request.get_json()
+    
+    if 'name' in data and 'last_name' in data:
+        if is_created := af.create_user(data['name'], data['last_name']):
+            return jsonify(is_created), 201
+        return jsonify({'error': 'Intenal Error', 'message': 'There was promlem while creating to database'}), 500
+    return jsonify({'error': 'Bad Request','message': 'Missing values to create user'}), 400
 
 
 @app.route('/users/<int:user_id>', methods=['PUT'])
--- src/api_functions.py
+++ src/api_functions.py
@@ -13,6 +13,18 @@ def get_user(user_id):
 
 
 def create_user(name, last_name):
-    conn = db.create_conn("users.db")
-    conn["users"].insert({"name": name, "last_name": last_name})
+    try:
+        conn = db.create_conn("users.db")
+        conn["users"].insert({"name": name, "last_name": last_name})
+        return {"message": "User created successfully"}
+    except Exception as e:
+        return {"message": str(e)}
 
+
+def edit_user(user_id, name, last_name):
+    try:
+        conn = db.create_conn("users.db")
+        conn["users"].update({"name": name, "last_name": last_name}, doc_ids=[user_id])
+        return {"message": "User updated successfully"}
+    except Exception as e:
+        return {"message": str(e)}
