c0b15293e8849cbe134ee8cbeb2d88afb383d197
nokokiii <kubanokoki@gmail>
2023-11-29 11:02:39 +0100

    run test

---
+++ src/tests/run.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src.tests.FlaskIntegrationTest import FlaskIntegrationTest
+from src.tests.FlaskUnitTest import FlaskUnitTest
+
+if __name__ == '__main__':
+    loader = unittest.TestLoader()
+    
+    suite = loader.loadTestsFromTestCase(FlaskIntegrationTest)
+    suite.addTests(loader.loadTestsFromTestCase(FlaskUnitTest))
+
+    runner = unittest.TextTestRunner()
+    result = runner.run(suite)
+    
